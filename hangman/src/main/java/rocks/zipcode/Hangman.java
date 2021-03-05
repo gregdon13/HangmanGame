6ebad349aa33a79f3ce3925f148f01b75315a7a0
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-05 08:44:12 -0500

    generates random word from array of strings. Array size can be changed.

--- hangman/src/main/java/rocks/zipcode/Hangman.java
+++ hangman/src/main/java/rocks/zipcode/Hangman.java
@@ -5,5 +5,11 @@ import java.util.Random;
 
 // Gregory Donnelly
 public class Hangman {
-
+    public String ranWordFromList () {
+        String[] randomWords = {"help", "book", "cash", "dome", "kelp", "drum", "pint", "mash", "roam"};
+        Integer randInt = Math.random() * ((randomWords.length) - 0 + 1) + 0;
+        String randWord = randomWords[randInt];
+        System.out.println(randWord);
+        return randWord;
+    }
 }
