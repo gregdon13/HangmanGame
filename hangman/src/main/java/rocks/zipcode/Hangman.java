ef0ecc75b43d33d720b16b7615dc220026015a82
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-05 09:03:10 -0500

    Theoretically I have created a placeholder that the user will see showing the number of dashes equal to a dynamically lengthed word generated in the random word generator. Generator does a math.random to to find a random number between 0 and the length of string array of words. Random word is the array of strings sub random number. Then splits to char array.

--- hangman/src/main/java/rocks/zipcode/Hangman.java
+++ hangman/src/main/java/rocks/zipcode/Hangman.java
@@ -5,11 +5,20 @@ import java.util.Random;
 
 // Gregory Donnelly
 public class Hangman {
-    public String ranWordFromList () {
-        String[] randomWords = {"help", "book", "cash", "dome", "kelp", "drum", "pint", "mash", "roam"};
+    public char[] userPlaceholderDashes () {
+        char[] dashes = [ranWordFromListChars().length];
+        for (int i = 0; i < dashes.length; i++) {
+            dashes[i] = '-';
+        }
+        return dashes;
+    }
+    public char[] ranWordFromListChars () {
+        String[] randomWords = {"pin", "help", "split", "stumpy"};
         Integer randInt = Math.random() * ((randomWords.length) - 0 + 1) + 0;
-        String randWord = randomWords[randInt];
-        System.out.println(randWord);
-        return randWord;
+        String randStr = randomWords[randInt];
+        System.out.println(randStr);
+        char[] charArrStr = randStr.toCharArray();
+        System.out.println(charArrStr);
+        return charArrStr;
     }
 }
