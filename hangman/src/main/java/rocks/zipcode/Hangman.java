2139ba80ef9ea367a396d899c2db5e43829c8d01
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-06 10:09:35 -0500

    now creates dash array of same length of wordStrArr

--- hangman/src/main/java/rocks/zipcode/Hangman.java
+++ hangman/src/main/java/rocks/zipcode/Hangman.java
@@ -1,5 +1,6 @@
 package rocks.zipcode;
 
+import java.util.Locale;
 import java.util.Scanner;
 import java.util.Random;
 
@@ -7,6 +8,16 @@ import java.util.Random;
 public class Hangman {
     String[] wordBank = {"pens", "shot", "tick", "lamp"};
     String wordStr = wordBank[randomInteger()];
+    char[] wordStrArr = wordStr.toCharArray();
+    char[] dashesArr = dashMaker(wordStrArr);
+
+    private char[] dashMaker(char[] wordStrArr) {
+        char[] dashArrMade = wordStrArr;
+        for (int i = 0; i < wordStrArr.length; i++) {
+            wordStrArr[i] = '-';
+        }
+        return dashArrMade;
+    }
 
     private int randomInteger() {
         int integer = (int) (Math.random() * wordBank.length);
