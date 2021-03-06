52a229ad47809bc5a4a7b392fd6f73024cf891b9
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-06 10:03:39 -0500

    wordbank made of same length words. String of the word is equal to the sub index of a random integer

--- hangman/src/main/java/rocks/zipcode/Hangman.java
+++ hangman/src/main/java/rocks/zipcode/Hangman.java
@@ -5,5 +5,12 @@ import java.util.Random;
 
 //Gregory Donnelly
 public class Hangman {
+    String[] wordBank = {"pens", "shot", "tick", "lamp"};
+    String wordStr = wordBank[randomInteger()];
+
+    private int randomInteger() {
+        int integer = (int) (Math.random() * wordBank.length);
+        return integer;
+    }
 
 }
